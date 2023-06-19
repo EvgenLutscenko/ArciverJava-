a869651a4176da06a29cbe7b08a3381b27c5e1c3
Evgenii <evgenlutscenko@icloud.com>
2023-06-19 10:21:14 +0200

    Archiver is complete

---
+++ src/main/java/org/example/command/ExitCommand.java
@@ -0,0 +1,11 @@
+package org.example.command;
+
+
+import org.example.ConsoleHelper;
+
+public class ExitCommand implements Command {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("До встречи!");
+    }
+}
